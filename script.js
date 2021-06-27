a40729a3d8781e625cae80e1911fdc6a2b5d34ab
Keneth Hu <hukeneth@gmail.com>
2021-06-27 21:58:27 +0000

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute() {
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     if(principal > 0 ) {
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+        document.getElementById("result").innerHTML="If you deposit \<mark>"+principal+",\</mark>\<br\>at an interest rate of \<mark>"+rate+"%\</mark>\<br\>You will receive an amount of \<mark>"+interest+",\</mark>\<br\>in the year \<mark>"+year+"\</mark>\<br\>";
     }	else {
             alert("Enter a positive number");
         document.getElementById("principal").focus()
